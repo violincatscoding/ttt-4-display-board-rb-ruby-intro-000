dc4d6cdd6d9badaf954f6199cd2a4e16c39b7245
diplomatic-chain-5587 <janiceviolin@gmail.com>
2020-05-29 22:58:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,5 @@ def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts board[4] = "X"
 end
 display_board(board)
